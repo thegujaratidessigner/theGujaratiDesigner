e8358e10a8674e3cf584a2bba197af6bc191df1b
The Gujarati Designer <thegujaratidesigner@gmail.com>
2026-05-05 22:40:18 +0530

    test(startbuilder): test route without hyphen to diagnose issue

---
+++ app/startbuilder/page.tsx
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic";
+
+export default function StartBuilderPage() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "system-ui" }}>
+      <h1>Start Builder Test</h1>
+      <p>If you see this, the route works without hyphen.</p>
+    </div>
+  );
+}
